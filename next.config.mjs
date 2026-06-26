2dc77c1837ae79c84ebea9935ac218425d9c1ced
rifat76511 <rifatalvi765@gmail.com>
2026-06-26 10:33:28 +0600

    feat: add image hostnames to next.config.mjs for remote image support

--- next.config.mjs
+++ next.config.mjs
@@ -16,12 +16,47 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "*.ibb.co",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "lh3.googleusercontent.com",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.imgbb.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
         pathname: "/**",
       },
     ],
